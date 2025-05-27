7cf62f4aec8d640174702963474fae633d85f92f
Joao jardim <joaopedrojardim1205@gmail.com>
2025-05-26 21:36:06 -0300

    commit

---
+++ Listas e Tuplas/listas.py
@@ -0,0 +1,23 @@
+frutas = ['maca', 'banana', 'laranja']
+print(frutas[0])
+print(frutas[1])
+print(frutas[-1])
+
+frutas = ['maca', 'banana', 'laranja']
+frutas[1] = 'morango'
+print(frutas)
+
+numero = [1, 2, 3]
+numero.append(4)
+print(numero)
+
+numero.remove(2)
+print(numero)
+
+numero.pop(0)
+print(numero)
+
+numero.insert(1, 5)
+print(numero)
+
+print(len(numero))
---
+++ Listas e Tuplas/tuplas.py
@@ -0,0 +1,10 @@
+cores = ('Vermelho', 'Verde', 'Azul')
+print(cores[0])
+print(cores[-1])
+
+cores = ('Vermelho', 'Verde', 'Azul')
+cores[1] = 'amarelo' #Erro tuplas imutáveis
+
+valores = (1, 2, 3, 4)
+print(valores.count(2))
+print(valores.index(0))
